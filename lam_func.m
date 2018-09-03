5ccba40b75462d065c062d159ebf5dd7cc93aead
cibinjoseph <cibinjoseph92@gmail.com>
2018-09-04 02:49:04 +0530

    Add climb velocity

--- lam_func.m
+++ lam_func.m
@@ -1,7 +1,7 @@
 function res=lam_func(lam)
-global theta root_cut tip_cut alf_disk mu sol;
+global theta root_cut tip_cut lam_c alf_disk mu sol;
 
 dCT=@(r_bar,psi)0.5*sol*((r_bar+mu*sin(psi)).^2).*(theta-lam./r_bar);
 CT=integral2(dCT,root_cut,tip_cut,0,2*pi);
-res=lam-mu*tan(alf_disk)-0.5*CT*(mu*mu+lam*lam)^(-0.5);
+res=lam-mu*tan(alf_disk)-0.5*CT*(mu*mu+lam*lam)^(-0.5)-lam_c*cos(alf_disk);
 return;
--- rotor_params.m
+++ rotor_params.m
@@ -1,5 +1,5 @@
 % Specified Rotor and Aerodynamic Parameters
-global root_cut tip_cut alf_disk theta mu sol;
+global root_cut tip_cut lam_c alf_disk theta mu sol;
 
 R = 8.0;
 c =1.0;
@@ -9,7 +9,9 @@ a = 2*pi;%5.74;          % d_Cl/d_alpha in radians
 rho = 1.2;               % in kg/m3
 root_cut = 0.05;         % in r/R
 tip_cut = 0.95;          % For accounting tip loss
-vel_forward = 10.0;
+vel_forward = 0.0;
+vel_climb = 0.0;
+
 alf_disk = 2*pi/180;
 theta=8*pi/180;
 
@@ -17,3 +19,4 @@ theta=8*pi/180;
 sol = Nb*c/(pi*R);
 Om = Om_rpm*pi/30;
 mu = vel_forward/(R*Om)*cos(alf_disk);
+lam_c = vel_climb/(R*Om);
