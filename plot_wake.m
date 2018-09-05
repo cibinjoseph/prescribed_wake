db6fda1dfdde136ffb1a6d547212cccc90888084
cibinjoseph <cibinjoseph92@gmail.com>
2018-09-05 23:16:27 +0530

    Add second blade wake for Landgrebe model

--- plot_wake.m
+++ plot_wake.m
@@ -15,9 +15,23 @@ switch option
     plot3(x_tip,y_tip,z_tip,'b-');
     hold on;
     plot3(x_root,y_root,z_root,'m-');
-    
     for i=1:length(x_tip)
       line([x_root(i) x_tip(i)],[y_root(i) y_tip(i)],[z_root(i) z_tip(i)]);
+    end
+    
+  case 5    % Tip, root wake and intermediate lines for TWO blades
+    plot3(x_tip,y_tip,z_tip,'b-');
+    hold on;
+    plot3(x_root,y_root,z_root,'m-');
+    for i=1:length(x_tip)
+      line([x_root(i) x_tip(i)],[y_root(i) y_tip(i)],[z_root(i) z_tip(i)]);
+    end
+    
+    % Second blade
+    plot3(-x_tip,-y_tip,z_tip,'b-');
+    plot3(-x_root,-y_root,z_root,'m-');
+    for i=1:length(x_tip)
+      line([-x_root(i) -x_tip(i)],[-y_root(i) -y_tip(i)],[z_root(i) z_tip(i)],'Color','g');
     end
     
   otherwise
@@ -27,6 +41,10 @@ end
 % Plot blade location
 hold on
 line([0 x_tip(1)],[0 y_tip(1)],[0 z_tip(1)],'Color','red','LineWidth',1.5);
+
+if (option==5)
+  line([0 -x_tip(1)],[0 -y_tip(1)],[0 z_tip(1)],'Color','red','LineWidth',1.5);
+end
 
 grid on;
 axis equal;
